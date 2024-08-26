1e3f2934f410a7d830d9a41f792e44bd9284cdca
Utkarsh4517 <uttkarsh.dav@gmail.com>
2024-08-26 19:52:01 +0530

    created controller to get all blogs

--- src/main/java/com/example/blogbackend/controller/BlogController.java
+++ src/main/java/com/example/blogbackend/controller/BlogController.java
@@ -1,9 +1,29 @@
 package com.example.blogbackend.controller;
 
+import com.example.blogbackend.model.Blog;
+import com.example.blogbackend.service.BlogService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/blogs")
 public class BlogController {
+
+    // Controller talks with service,
+    // Service talks with Repository,
+    // Repository talks with db, according to the model.
+
+    @Autowired
+    private BlogService blogService;
+
+    @GetMapping // at /api/blogs
+    public List<Blog> getAllBlogs() {
+        return blogService.getAllBlogs();
+    }
+
+
 }

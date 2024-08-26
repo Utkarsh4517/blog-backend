7005739e3b9b5162c497fabbda6119b01f3b94ee
Utkarsh4517 <uttkarsh.dav@gmail.com>
2024-08-26 19:30:26 +0530

    created service to get all blogs

--- src/main/java/com/example/blogbackend/service/BlogService.java
+++ src/main/java/com/example/blogbackend/service/BlogService.java
@@ -1,7 +1,19 @@
 package com.example.blogbackend.service;
 
+import com.example.blogbackend.model.Blog;
+import com.example.blogbackend.repository.BlogRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class BlogService {
+
+    @Autowired
+    private BlogRepository blogRepository;
+
+    public List<Blog> getAllBlogs() {
+        return blogRepository.findAll();
+    }
 }

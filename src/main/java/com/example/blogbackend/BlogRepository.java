271434f49084cc0d9f82562399765bbb79f708d2
Utkarsh4517 <uttkarsh.dav@gmail.com>
2024-08-26 19:13:15 +0530

    added comms

--- src/main/java/com/example/blogbackend/BlogRepository.java
+++ src/main/java/com/example/blogbackend/BlogRepository.java
@@ -5,4 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BlogRepository extends JpaRepository<Blog, Long> {
     // Any additional queries if needed.
+    // JPA Provides basic crud by default
 }
